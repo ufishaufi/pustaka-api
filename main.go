542779792678d5b3b615066232defabf6b4f6209
Muhammad Shaufi Imanulhaq <shaufiman@gmail.com>
2022-10-03 13:48:03 +0800

    Handler

--- main.go
+++ main.go
@@ -9,19 +9,23 @@ import (
 func main() {
 	router := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"name": "Shaufi Imanulhaq",
-			"bio":  "A Software engineer & content creator",
-		})
-	})
+	router.GET("/", rootHandler)
 
-	router.GET("/hello", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"title":    "Hello World",
-			"subtitle": "Belajar Golang bareng Shaufi Imanulhaq",
-		})
-	})
+	router.GET("/hello", helloHandler)
 
-	router.Run(":8888")
+	router.Run()
+}
+
+func rootHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"name": "Shaufi Imanulhaq",
+		"bio":  "A Software engineer & content creator",
+	})
+}
+
+func helloHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"title":    "Hello World",
+		"subtitle": "Belajar Golang bareng Shaufi Imanulhaq",
+	})
 }
