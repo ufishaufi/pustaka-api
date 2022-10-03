cac51c6c67f59427b30ff24df471fa2cf8b64d57
Muhammad Shaufi Imanulhaq <shaufiman@gmail.com>
2022-10-03 14:17:45 +0800

    Path Variable and Query String

--- main.go
+++ main.go
@@ -10,8 +10,9 @@ func main() {
 	router := gin.Default()
 
 	router.GET("/", rootHandler)
-
 	router.GET("/hello", helloHandler)
+	router.GET("/books/:id/:title", booksHandler)
+	router.GET("/query", queryHandler)
 
 	router.Run()
 }
@@ -28,4 +29,19 @@ func helloHandler(c *gin.Context) {
 		"title":    "Hello World",
 		"subtitle": "Belajar Golang bareng Shaufi Imanulhaq",
 	})
+}
+
+func booksHandler(c *gin.Context) {
+	id := c.Param("id")
+	title := c.Param("title")
+
+	c.JSON(http.StatusOK, gin.H{"id": id, "title": title})
+
+}
+
+func queryHandler(c *gin.Context) {
+	title := c.Query("title")
+	price := c.Query("price")
+
+	c.JSON(http.StatusOK, gin.H{"title": title, "price": price})
 }
