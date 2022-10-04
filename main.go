7945e2e13a14f9574290ea5a1336238085d7f31a
Muhammad Shaufi Imanulhaq <shaufiman@gmail.com>
2022-10-04 10:58:18 +0800

    HTTP Post and JSON Binding

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -13,6 +14,8 @@ func main() {
 	router.GET("/hello", helloHandler)
 	router.GET("/books/:id/:title", booksHandler)
 	router.GET("/query", queryHandler)
+
+	router.POST("/books", postBooksHandler)
 
 	router.Run()
 }
@@ -44,4 +47,26 @@ func queryHandler(c *gin.Context) {
 	price := c.Query("price")
 
 	c.JSON(http.StatusOK, gin.H{"title": title, "price": price})
+}
+
+type BookInput struct {
+	Title    string
+	Price    int
+	SubTitle string `json:"sub_title"`
+}
+
+func postBooksHandler(c *gin.Context) {
+	var bookInput BookInput
+
+	err := c.ShouldBindJSON(&bookInput)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"title":     bookInput.Title,
+		"price":     bookInput.Price,
+		"sub_title": bookInput.SubTitle,
+	})
 }
