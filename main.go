1889533742b380e3acbdbfd46bf695d2fa976caa
Muhammad Shaufi Imanulhaq <shaufiman@gmail.com>
2022-10-19 01:30:01 +0800

    Auto Migration

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"pustaka-api/book"
 	"pustaka-api/handler"
 
 	"github.com/gin-gonic/gin"
@@ -12,12 +12,12 @@ import (
 
 func main() {
 	dsn := "root:Shaufi098709@tcp(127.0.0.1:3306)/pustaka_api?charset=utf8mb4&parseTime=True&loc=Local"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Db connection error")
 	}
 
-	fmt.Println("Database connection succedd")
+	db.AutoMigrate(&book.Book{})
 
 	router := gin.Default()
 
